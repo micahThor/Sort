771d40729ffb1fe7badb78425cabeddcf6e9d49a
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2020-02-10 18:52:04 -0800

    initial build

---
+++ src/test/java/com/micahThor/sort/SortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micahThor.sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
